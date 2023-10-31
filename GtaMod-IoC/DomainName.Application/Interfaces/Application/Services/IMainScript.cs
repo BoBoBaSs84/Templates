f79dce10ba9c55bf65bebe10cb3188f3af44386e
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-10-31 13:48:55 +0100

    this is it!

--- GtaMod-IoC/DomainName.Application/Interfaces/Application/Services/IMainScript.cs
+++
@@ -1,9 +0,0 @@
-﻿using DomainName.Application.Interfaces.Application.Services.Base;
-
-namespace DomainName.Application.Interfaces.Application.Services;
-
-/// <summary>
-/// The main script interface.
-/// </summary>
-public interface IMainScript : IScriptServiceBase
-{ }
