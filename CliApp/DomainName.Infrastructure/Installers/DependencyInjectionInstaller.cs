41c51f230f4a0ddcea4d264e2c4b4ebc8e20ba86
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2023-11-07 17:17:12 +0100

    feat: console app template
    changes:
    - cli template added
    
    closes #23

---
+++ CliApp/DomainName.Infrastructure/Installers/DependencyInjectionInstaller.cs
@@ -0,0 +1,23 @@
+﻿using DomainName.Infrastructure.Extensions;
+
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DomainName.Infrastructure.Installers;
+
+/// <summary>
+/// The infrastructure dependency injection class.
+/// </summary>
+public static class DependencyInjectionInstaller
+{
+	/// <summary>
+	/// Adds the infrastructure services to the service collection.
+	/// </summary>
+	/// <param name="services">The service collection to enrich.</param>
+	/// <returns>The enriched service collection.</returns>
+	public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+	{
+		services.AddLoggerService();
+
+		return services;
+	}
+}
