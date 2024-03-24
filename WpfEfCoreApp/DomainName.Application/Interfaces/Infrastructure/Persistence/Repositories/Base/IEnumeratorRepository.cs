ad5b2d96e975abddb0bffe24bbf0f5b553d8eba0
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-03-24 12:05:08 +0100

    feat: enumerator repository
    changes:
    - added `IEnumeratorModelBase` interface
    - added `EnumeratorModelBase` class
    - added `IEnumeratorRepository` interface
    - added `EnumeratorRepository` class
    
    closes #41

---
+++ WpfEfCoreApp/DomainName.Application/Interfaces/Infrastructure/Persistence/Repositories/Base/IEnumeratorRepository.cs
@@ -0,0 +1,94 @@
+﻿using DomainName.Domain.Interfaces.Models.Base;
+
+namespace DomainName.Application.Interfaces.Infrastructure.Persistence.Repositories.Base;
+
+/// <summary>
+/// The enumerator repository interface.
+/// </summary>
+/// <typeparam name="T">The enumerator type work with.</typeparam>
+public interface IEnumeratorRepository<T> : IGenericRepository<T> where T : class, IEnumeratorModelBase
+{
+	/// <summary>
+	/// Returns an enumerator entity by its primary key.
+	/// </summary>
+	/// <param name="id">The primary key of the enumerator entity.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entity be tracked?</param>
+	/// <returns>The enumerator entity or <see langword="null"/>.</returns>
+	T? GetById(
+		int id,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false
+		);
+
+	/// <summary>
+	/// Returns an enumerator entity by its primary key.
+	/// </summary>
+	/// <param name="id">The primary key of the enumerator entity.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entity be tracked?</param>
+	/// <param name="cancellationToken">The cancellation token to cancel the request.</param>
+	/// <returns>The enumerator entity or <see langword="null"/>.</returns>
+	Task<T?> GetByIdAsync(
+		int id,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Returns a collection of enumerator entites by their primary keys.
+	/// </summary>
+	/// <param name="ids">The primary keys of the enumerator entities.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entities be tracked?</param>
+	/// <returns>A collection of enumerator entites.</returns>
+	IEnumerable<T> GetByIds(
+		IEnumerable<int> ids,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false
+		);
+
+	/// <summary>
+	/// Returns a collection of enumerator entites by their primary keys.
+	/// </summary>
+	/// <param name="ids">The primary keys of the enumerator entities.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entities be tracked?</param>
+	/// <param name="cancellationToken">The cancellation token.</param>
+	/// <returns>A collection of enumerator entites.</returns>
+	Task<IEnumerable<T>> GetByIdsAsync(
+		IEnumerable<int> ids,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false,
+		CancellationToken cancellationToken = default
+		);
+
+	/// <summary>
+	/// Returns an enumerator entity by its name.
+	/// </summary>
+	/// <param name="name">The name of the enumerator entity.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entity be tracked?</param>
+	/// <returns>The enumerator entity or <see langword="null"/>.</returns>
+	T? GetByName(
+		string name,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false
+		);
+
+	/// <summary>
+	/// Returns an enumerator entity by its name.
+	/// </summary>
+	/// <param name="name">The name of the enumerator entity.</param>
+	/// <param name="ignoreQueryFilters">Should model-level entity query filters be applied?</param>
+	/// <param name="trackChanges">Should the fetched entity be tracked?</param>
+	/// <param name="cancellationToken">The cancellation token to cancel the request.</param>
+	/// <returns>The enumerator entity or <see langword="null"/>.</returns>
+	Task<T?> GetByName(
+		string name,
+		bool ignoreQueryFilters = false,
+		bool trackChanges = false,
+		CancellationToken cancellationToken = default
+		);
+}
---
+++ WpfEfCoreApp/DomainName.Infrastructure/Persistence/Repositories/Base/EnumeratorRepository.cs
@@ -0,0 +1,79 @@
+﻿using DomainName.Application.Interfaces.Infrastructure.Persistence.Repositories.Base;
+using DomainName.Domain.Models.Base;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace DomainName.Infrastructure.Persistence.Repositories.Base;
+
+/// <summary>
+/// The enumerator repository class.
+/// </summary>
+/// <inheritdoc/>
+internal abstract class EnumeratorRepository<T>(DbContext dbContext) : GenericRepository<T>(dbContext), IEnumeratorRepository<T> where T : EnumeratorModelBase
+{
+	public T? GetById(int id, bool ignoreQueryFilters = false, bool trackChanges = false)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => x.Id.Equals(id),
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return query.SingleOrDefault();
+	}
+
+	public async Task<T?> GetByIdAsync(int id, bool ignoreQueryFilters = false, bool trackChanges = false, CancellationToken cancellationToken = default)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => x.Id.Equals(id),
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return await query.SingleOrDefaultAsync(cancellationToken);
+	}
+
+	public IEnumerable<T> GetByIds(IEnumerable<int> ids, bool ignoreQueryFilters = false, bool trackChanges = false)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => ids.Contains(x.Id),
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return [.. query];
+	}
+
+	public async Task<IEnumerable<T>> GetByIdsAsync(IEnumerable<int> ids, bool ignoreQueryFilters = false, bool trackChanges = false, CancellationToken cancellationToken = default)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => ids.Contains(x.Id),
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return await query.ToListAsync(cancellationToken);
+	}
+
+	public T? GetByName(string name, bool ignoreQueryFilters = false, bool trackChanges = false)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => x.Name == name,
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return query.SingleOrDefault();
+	}
+
+	public async Task<T?> GetByName(string name, bool ignoreQueryFilters = false, bool trackChanges = false, CancellationToken cancellationToken = default)
+	{
+		IQueryable<T> query = PrepareQuery(
+			expression: x => x.Name == name,
+			ignoreQueryFilters: ignoreQueryFilters,
+			trackChanges: trackChanges
+			);
+
+		return await query.SingleOrDefaultAsync(cancellationToken);
+	}
+}
