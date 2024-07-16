b81d7065691314308e4da1228a44e699762baf19
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-07-16 10:58:02 +0200

    chore(deps): nuget package updates

--- WpfEfCoreApp/DomainName.Domain/Models/ExampleModel.cs
+++ WpfEfCoreApp/DomainName.Domain/Models/ExampleModel.cs
@@ -9,11 +9,11 @@ namespace DomainName.Domain.Models;
 public sealed class ExampleModel : DataModelBase, IExampleModel
 {
 	/// <inheritdoc/>
-	public string FirstName { get; set; }
+	public string FirstName { get; set; } = default!;
 
 	/// <inheritdoc/>
-	public string LastName { get; set; }
+	public string LastName { get; set; } = default!;
 
 	/// <inheritdoc/>
-	public int Age { get; set; }
+	public int Age { get; set; } = default!;
 }
