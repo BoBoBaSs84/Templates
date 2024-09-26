ec63cff3418a393ac8a0f95388437c7778d4210e
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-09-26 17:57:08 +0200

    feat: missing commands added

--- WpfApp/DomainName.Application/ViewModels/MainViewModel.cs
+++ WpfApp/DomainName.Application/ViewModels/MainViewModel.cs
@@ -1,4 +1,7 @@
-﻿using DomainName.Application.Interfaces.Application.Services;
+﻿using BB84.Notifications.Commands;
+using BB84.Notifications.Interfaces.Commands;
+
+using DomainName.Application.Interfaces.Application.Services;
 using DomainName.Application.ViewModels.Base;
 
 namespace DomainName.Application.ViewModels;
@@ -9,8 +12,23 @@ namespace DomainName.Application.ViewModels;
 /// <param name="navigationService">The navigation service instance to use.</param>
 public sealed class MainViewModel(INavigationService navigationService) : ViewModelBase
 {
+	private IActionCommand? _aboutCommand;
+	private IActionCommand? _exitCommand;
+
 	/// <summary>
 	/// The navigation service instance.
 	/// </summary>
 	public INavigationService NavigationService => navigationService;
+
+	/// <summary>
+	/// The command to show the about control.
+	/// </summary>
+	public IActionCommand AboutCommand
+		=> _aboutCommand ??= new ActionCommand(NavigationService.NavigateTo<AboutViewModel>);
+
+	/// <summary>
+	/// The command to exit the application.
+	/// </summary>
+	public IActionCommand ExitCommand
+		=> _exitCommand ??= new ActionCommand(() => Environment.Exit(0));
 }
