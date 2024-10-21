fa24984530beff54a47519fc7e73f353e958c1b3
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-21 17:08:47 +0200

    feat: current user service added

---
+++ WinForms/DomainName.Application/Interfaces/Presentation/Services/ICurrentUserService.cs
@@ -0,0 +1,22 @@
+﻿namespace DomainName.Application.Interfaces.Presentation.Services;
+
+/// <summary>
+/// The interface for the current user service.
+/// </summary>
+public interface ICurrentUserService
+{
+	/// <summary>
+	/// The current user name.
+	/// </summary>
+	string UserName { get; }
+
+	/// <summary>
+	/// The current user domain name.
+	/// </summary>
+	string UserDomainName {  get; }
+
+	/// <summary>
+	/// The current user machine name.
+	/// </summary>
+	string MachineName {  get; }
+}
---
+++ WinForms/DomainName.Presentation/Services/CurrentUserService.cs
@@ -0,0 +1,18 @@
+﻿using DomainName.Application.Interfaces.Presentation.Services;
+
+namespace DomainName.Presentation.Services;
+
+/// <summary>
+/// The current user service.
+/// </summary>
+internal sealed class CurrentUserService : ICurrentUserService
+{
+	/// <inheritdoc/>
+	public string UserName => Environment.UserName;
+
+	/// <inheritdoc/>
+	public string UserDomainName => Environment.UserDomainName;
+
+	/// <inheritdoc/>
+	public string MachineName => Environment.MachineName;
+}
