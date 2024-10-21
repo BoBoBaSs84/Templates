f2a2f8641a1ccf1ed91043ab82d65086be21d0fb
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2024-10-21 17:58:19 +0200

    feat: some refactoring applied

--- WinForms/DomainName.Presentation/Forms/MainForm.cs
+++ WinForms/DomainName.Presentation/Forms/MainForm.cs
@@ -9,21 +9,25 @@ namespace DomainName.Presentation.Forms;
 /// </summary>
 public partial class MainForm : Form
 {
+	private readonly ICurrentUserService _currentUserService;
 	private readonly INotificationService _notificationService;
 
 	/// <summary>
 	/// Initializes a new instance of the <see cref="MainForm"/> class.
 	/// </summary>
+	/// <param name="currentUserService">The current user service instance to use.</param>
 	/// <param name="notificationService">The notification service instance to use.</param>
-	public MainForm(INotificationService notificationService)
+	public MainForm(ICurrentUserService currentUserService, INotificationService notificationService)
 	{
-		_notificationService = notificationService;
 		InitializeComponent();
+
+		_currentUserService = currentUserService;
+		_notificationService = notificationService;
 	}
 
 	private void exitToolStripMenuItem_Click(object sender, EventArgs e)
 	{
-		_notificationService.SendInformation("Bye!");
+		_notificationService.SendInformation($"Bye, {_currentUserService.UserName}");
 		FormsApplication.Exit();
 	}
 }
