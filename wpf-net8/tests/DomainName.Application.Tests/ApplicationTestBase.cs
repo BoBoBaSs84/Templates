d45f6751e2c1fed91423aab41e809c7a619781c8
BoBoBaSs84 <73112377+BoBoBaSs84@users.noreply.github.com>
2025-10-24 16:23:59 +0200

    chore(deps): nuget packages for net8 wpf template updated

--- wpf-net8/tests/DomainName.Application.Tests/ApplicationTestBase.cs
+++ wpf-net8/tests/DomainName.Application.Tests/ApplicationTestBase.cs
@@ -2,18 +2,4 @@
 
 [TestClass]
 public abstract class ApplicationTestBase
-{
-	private static TestContext? s_context;
-
-	[AssemblyInitialize]
-	public static void AssemblyInitialize(TestContext context)
-		=> s_context = context;
-
-	[TestInitialize]
-	public void TestInitialize()
-		=> s_context?.WriteLine($"Initialize {s_context.TestName} ..");
-
-	[TestCleanup]
-	public void TestCleanup()
-		=> s_context?.WriteLine($"Cleanup {s_context.TestName} ..");
-}
+{ }
